3e8064c3c98b5186742b486ef877790de6ee38d9
komus-Israel <komolehinisrael@gmail.com>
2024-04-23 12:31:16 +0100

    Setup physical device selection

--- VulkanTutorial/physicalDeviceHandler.hpp
+++ VulkanTutorial/physicalDeviceHandler.hpp
@@ -18,13 +18,51 @@ private:
     
 public:
     
-    
     //  The graphics card that will be selected will be stored in this handle
-    static constexpr VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
     
     void pickPhysicalDevice(VkInstance instance) {
+        
+        
+        
+        
+        //  Query the number of physical devices
         vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, nullptr);
-        std::cout << "Header Working" << std::endl;
+        
+        //  validate the gpu count
+        if (physicalDeviceCount == 0) {
+            std::runtime_error("Failed to Locate GPU with Vulkan support");
+        }
+        
+        //  Declare a vector to hold all physical devices
+        std::vector<VkPhysicalDevice> physicalDevices(physicalDeviceCount);
+        vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, physicalDevices.data());
+        
+        
+        //  log out the number of detected gpus with vulkan support
+        std::cout << "Number of discovered GPUs: " << physicalDeviceCount << std::endl;
+        
+        // If any of the GPUs meet the requirement, that GPU is selected
+        for (const auto& device : physicalDevices) {
+            
+            if (isDeviceSuitable(device)) {
+                physicalDevice = device;
+                break;
+            }
+            
+        }
+        
+        //  Check if the physical device handler is still null
+        if (physicalDevice == VK_NULL_HANDLE){
+            std::runtime_error("Failed to find a suitable GPU");
+        } else {
+            std::cout << "Suitable GPU found" << std::endl;
+        }
+        
+    }
+    
+    bool isDeviceSuitable(VkPhysicalDevice physicalDevice) {
+        return true;
     }
     
 };
