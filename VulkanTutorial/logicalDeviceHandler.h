c2c7093592a3e737f8e5f9d82b107526d202bb71
komus-Israel <komolehinisrael@gmail.com>
2024-04-24 13:56:38 +0100

    Updated logical device for present queue

--- VulkanTutorial/logicalDeviceHandler.h
+++ VulkanTutorial/logicalDeviceHandler.h
@@ -1,6 +1,7 @@
 #ifndef logicalDeviceHandler_h
 #define logicalDeviceHandler_h
 #include "queueFamiliesHandler.h"
+#include <set>
 
 class LogicalDeviceHandler {
     
@@ -19,7 +20,12 @@ public:
     VkDevice device;
     
     //  The queues are automatically created along with the logical device
+    
+    //  graphics queue handler
     VkQueue graphicsQueue;
+    
+    //  presentation queue handler
+    VkQueue presentQueue;
     
     void createLogicalDevice(VkPhysicalDevice physicalDevice, VkSurfaceKHR surface){
         
@@ -29,18 +35,25 @@ public:
         
         QueueFamiliesHandler::QueueFamilyIndices indices = queueFamiliesHandler.findQueueFamilies(physicalDevice, surface);
         
-        VkDeviceQueueCreateInfo queueCreateInfo{};
-        
-        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
-        queueCreateInfo.queueCount = 1;
+        std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+        std::set<uint32_t> uniqueQueueFamilies = { indices.graphicsFamily.value(), indices.presentFamily.value() };
         
         //  Vulkan allows to assign priorities to queues to influence the scheduling of
         //  command buffer execution using floating point numbers between 0.0 and 1.0
         //  This is required even if there is a single queue
-        
         float queuePriority = 1.0f;
-        queueCreateInfo.pQueuePriorities = &queuePriority;
+        
+        for (uint32_t queueFamily : uniqueQueueFamilies) {
+            
+            VkDeviceQueueCreateInfo queueCreateInfo{};
+            queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+            queueCreateInfo.queueFamilyIndex = queueFamily;
+            queueCreateInfo.queueCount = 1;
+            queueCreateInfo.pQueuePriorities = &queuePriority;
+            queueCreateInfos.push_back(queueCreateInfo);
+
+            
+        }
         
         //  Specifying the device features that will be used
         
@@ -53,8 +66,8 @@ public:
         createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
         
         // First add pointers to the queue creation info and device features structs
-        createInfo.pQueueCreateInfos = &queueCreateInfo;
-        createInfo.queueCreateInfoCount = 1;
+        createInfo.pQueueCreateInfos = queueCreateInfos.data();
+        createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
         createInfo.pEnabledFeatures = &deviceFeatures;
         
         // Instantiate the logical device
@@ -64,6 +77,7 @@ public:
         }
         
         vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+        vkGetDeviceQueue(device, indices.presentFamily.value(), 0, &presentQueue);
         
     }
     
